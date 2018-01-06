1933324b6849801a5fd1d72e5fa9c11968ec6e30
Justin <jl2378@cornell.edu>
2018-01-06 13:13:29 -0800

    Readd VS Macros. C++ nullptr convention change

--- drvmap/main.cpp
+++ drvmap/main.cpp
@@ -38,7 +38,7 @@ int __stdcall main(const int argc, char** argv)
 	drvmap::util::open_binary_file(argv[1], driver_image);
 	drvmap::drv_image driver(driver_image);
 
-	const auto kernel_memory = capcom->allocate_pool(driver.size(), kernel::NonPagedPool, true, NULL);
+	const auto kernel_memory = capcom->allocate_pool(driver.size(), kernel::NonPagedPool, true, nullptr);
 
 	assert(kernel_memory != 0);
 
